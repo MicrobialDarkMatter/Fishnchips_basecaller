47eb52810f63b6ec6b10e4adfb4ea59091c78707
Miro <miropakanec@gmail.com>
2021-01-30 15:21:33 +0100

    removed redudntant scripts (added discard flag to test)

--- test_fishnchips.py
+++ test_fishnchips.py
@@ -7,6 +7,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', required=True, help='Config filepath.')
     parser.add_argument('-n', '--name', required=False, help='Name of the experiment. If none provided, name with a timestamp will be assigned.')
+    parser.add_argument('-d', '--discard', required=False, default=False, action='store_true', help='Discard existing trained model (if one exists) and begin training from scratch.')
     parser.add_argument('-w', '--warnings', required=False, default=False, action='store_true', help='Display tensorflow warning.')
     args = parser.parse_args()
     verify_args(args)
@@ -22,12 +23,12 @@ def set_logging(args):
         tf.get_logger().setLevel('ERROR')
         os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-def main(config_path, experiment_name):
+def main(config_path, experiment_name, discard_existing):
     config = api.get_config(config_path)
     api.setup_experiment(experiment_name)
-    api.train(config, experiment_name)
+    api.test(config, experiment_name, discard_existing)
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.config, args.name)
-    print(' - Script has finished successfully.')
+    main(args.config, args.name, args.discard)
+    print(' - Script has finished successfully.') 
